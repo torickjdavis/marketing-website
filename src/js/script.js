596b735ae300f39bfb0e18e53f8fa4e1bae2c669
Torick Davis <torickjdavis@gmail.com>
2019-02-11 05:05:40 +0000

    completed desktop and mobile styles

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   let navButtons = document.querySelectorAll(".toggle-menu");
   for(let i = 0; i < navButtons.length; i++) {
     navButtons[i].addEventListener("click", (event) => {
-      event.target.parentElement.parentElement.classList.toggle("active");
-      event.target.parentElement.classList.toggle("active");
+      let fontAwesomeIcon = document.createElement("i");
+      fontAwesomeIcon.classList.add("fas");
+      
+      let button = event.target.parentElement;
+      button.parentElement.classList.toggle("active");
+      button.classList.toggle("active");
+      
+      let buttonActive = document.querySelector(".toggle-menu.active");
+      if(buttonActive != undefined && buttonActive != null) {
+        fontAwesomeIcon.classList.add("fa-times");
+      }
+      else {
+        fontAwesomeIcon.classList.add("fa-bars");
+      }
+      
+      button.removeChild(button.firstChild);
+      button.appendChild(fontAwesomeIcon);
     });
   }
 });
